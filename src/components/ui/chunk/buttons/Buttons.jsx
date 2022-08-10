48d2df746a40872ce79066664717ea6705c7ea23
dmittryyyy <hhh963123741@gmail.com>
2022-08-10 10:56:20 +0700

    Close issue #5

--- src/components/ui/chunk/buttons/Buttons.jsx
+++ src/components/ui/chunk/buttons/Buttons.jsx
@@ -1,16 +1,14 @@
-import { React, useState } from 'react';
+import { React } from 'react';
 
 import './buttons.scss';
 
 export const Buttons = () => {
 
-  const [activeBtn, setActiveBtn] = useState(false);
-
   return (
     <div className="buttons">
       <div className="buttons__item">
         <span className='buttons__item-subtitle'>Интерактивная</span>
-        <button className="buttons__item-btn" disabled={activeBtn} onClick={() => setActiveBtn(true)}>Отправить</button>
+        <button className="buttons__item-btn">Отправить</button>
       </div>
       <div className="buttons__item">
         <span className='buttons__item-subtitle'>Ховер</span>
